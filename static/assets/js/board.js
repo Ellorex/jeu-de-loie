17814f90b11af5720c7cf2cbb1f13c23a737d68d
Lea <lea.rerolle@gmail.com>
2019-07-15 17:45:00 +0200

    position player pawn on specific square

--- static/assets/js/board.js
+++ static/assets/js/board.js
@@ -1,6 +1,9 @@
 let rollDices = document.getElementById('rollDices');
 let diceResult1 = document.getElementById('diceResult1');
 let diceResult2 = document.getElementById('diceResult2');
+var player1 = document.getElementById('player1');
+var player2 = document.getElementById('player2');
+var position;
 rollDices.addEventListener('click', () => {
     console.log('Lancer de dé');
     socket.emit('rool dices');
@@ -10,3 +13,13 @@ rollDices.addEventListener('click', () => {
         diceResult2.innerText = dices[1];
     })
 })
+
+function getNewPosition(dest) {
+    destination = document.getElementById(`case-${dest}`);
+    position = destination.getBoundingClientRect();
+
+    player1.style.top = position.top+"px";
+    player1.style.left = position.left+"px";
+}
+
+console.log(getNewPosition(57));
