2b4fc0c50a6343b82142d99231edf57423f8607a
Jessyv96 <c.j91350@gmail.com>
2019-07-15 11:06:29 +0200

    Server directory

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const socketio = require('socket.io');
 
 let app = express();
 const PORT = 8080;
+
+app.use(express.static(__dirname + '/static'));
+
 let server = app.listen(PORT, () => {
     console.log(`Server listen ${PORT}`)
 });
---
+++ server/utils/rollDice.js
@@ -0,0 +1,8 @@
+let dicesArray = [];
+
+let rollADice = () => {
+    let diceScore = Math.floor(Math.random()*6)+1;
+}
+
+
+console.log(Math.floor(Math.random()*6)+1);
