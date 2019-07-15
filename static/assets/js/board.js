aab6ece32fbbfe8ef99cc13c2474364a33d4f752
Jessyv96 <c.j91350@gmail.com>
2019-07-15 15:13:42 +0200

    ROLL DICES Event #3

--- static/assets/js/board.js
+++ static/assets/js/board.js
@@ -1,9 +1,12 @@
-let rollDices =  document.getElementById('rollDices');
-
+let rollDices = document.getElementById('rollDices');
+let diceResult1 = document.getElementById('diceResult1');
+let diceResult2 = document.getElementById('diceResult2');
 rollDices.addEventListener('click', () => {
     console.log('Lancer de dé');
     socket.emit('rool dices');
     socket.on('dice score', (data) => {
-        console.log(data)
+        let dices = data.diceScore;
+        diceResult1.innerText = dices[0];
+        diceResult2.innerText = dices[1];
     })
 })
